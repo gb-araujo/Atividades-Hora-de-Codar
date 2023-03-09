a40704fe2201712d10371b1dc11aefc0a06261f4
Gabriel Araujo <86437334+gb-araujo@users.noreply.github.com>
2023-03-09 12:36:18 -0300

    Update atividades.js

--- Hora de codar 1 - JavaScript/atividades.js
+++ Hora de codar 1 - JavaScript/atividades.js
@@ -30,8 +30,8 @@
 
 /*  //RETÂNGULO
 
-    var base = prompt("Qual é o tamanho da base do retângulo? ");
-    var altura = prompt("E qual é a altura? ");
+    var base = parseInt(prompt("Qual é o tamanho da base do retângulo? "));
+    var altura = parseInt(prompt("E qual é a altura? "));
     var area_retangulo = base * altura;
     document.write(area_retangulo + "<br>");
 
@@ -39,7 +39,7 @@
 
 /* //QUADRADO
 
-    var lado = prompt("Qual é o tamanho do lado do quadrado? ");
+    var lado = parseInt(prompt("Qual é o tamanho do lado do quadrado? "));
     area_quadrado =  lado * lado;
     document.write(area_quadrado) 
 
@@ -47,8 +47,8 @@
 
 /* //LOSANGO
 
-    var diagonal_maior = prompt("Qual é a diagonal maior? ")
-    var diagonal_menor = prompt("Qual é a diagonal menor? ")
+    var diagonal_maior = parseInt(prompt("Qual é a diagonal maior? "));
+    var diagonal_menor = parseInt(prompt("Qual é a diagonal menor? "));
     area_los = (diagonal_maior * diagonal_menor) / 2;
     document.write(area_los)
 
@@ -66,8 +66,8 @@
 
 /* //PARALELOGRAMO
 
-    var b_paralelo = prompt("Qual é a base do Paralelogramo? ") ;
-    var a_pararelo = prompt("Qual é a altura do Paralelogramo? ");
+    var b_paralelo = parseInt(prompt("Qual é a base do Paralelogramo? ")) ;
+    var a_pararelo = parseInt(prompt("Qual é a altura do Paralelogramo? "));
     var area_pararelo = b_paralelo * a_pararelo;
     document.write(area_pararelo);
 
@@ -75,8 +75,8 @@
 
 /* //TRIANGULO
 
-    var b_triangulo = prompt("Qual é a base do triangulo?");
-    var a_triangulo = prompt("Qual é a altura do triangulo?");
+    var b_triangulo = parseInt(prompt("Qual é a base do triangulo?"));
+    var a_triangulo = parseInt(prompt("Qual é a altura do triangulo?"));
     var area_triangulo = (b_triangulo * a_triangulo) / 2;
     document.write(area_triangulo);
 
@@ -85,7 +85,7 @@
 /* //CIRCULO
 
     var pi = 3.14159;
-    var raio = prompt("Qual é o raio? ")
+    var raio = parseInt(prompt("Qual é o raio? "))
     area = pi * raio ** 2
     document.write(area)
     
