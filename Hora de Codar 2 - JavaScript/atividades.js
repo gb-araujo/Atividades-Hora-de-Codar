af1fd1c391f152d633ba573a8037e0e1a671949f
Gabriel Araujo <86437334+gb-araujo@users.noreply.github.com>
2023-03-09 11:54:15 -0300

    Add files via upload

---
+++ Hora de Codar 2 - JavaScript/atividades.js
@@ -0,0 +1,48 @@
+/* 
+// ---- Exercício 1 ----
+
+var numero1 = parseInt(prompt("Digite um número: "));
+var numero2 = parseInt(prompt("Digite outro número: "));
+    if (numero1 > numero2) {
+        document.write("O número 1 é maior. ");
+    }else if (numero2 > numero1) { 
+        document.write("O número 2 é maior ");
+    } else {
+        document.write("Os números são iguais. ");
+    }
+
+*/ 
+
+/*
+// ---- Exercício 2 ---- 
+
+var numero = parseFloat(prompt("Digite um número para verificar se é positivo ou negativo. "))
+    if (numero >= 0) {
+        document.write("O número é positivo")
+    } else {
+        document.write("O número é negativo.")
+    }
+
+*/
+
+// ---- Exercício 3  ---- 
+/*
+
+var num1 = parseInt(prompt("Digite um número: "))
+var num2 = parseInt(prompt("Digite um segundo número: "))
+var num3 = parseInt(prompt("Digite um terceiro número: "))
+    if (num1 > num2) {
+        document.write("O primeiro número é o maior")
+    }
+    else if (num1 > num3) {
+        document.write("O primeiro número é maior")
+    }
+    else if (num2 > num3) {
+        document.write("O segundo número é maior")
+    }
+
+    else {
+        document.write("O terceiro número é o maior")
+    }
+
+*/
